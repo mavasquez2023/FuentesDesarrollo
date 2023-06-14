23b1bf34754c131353a01402353cad7a194b76a9
hurojas-cyber <hugo.rojas.r@gmail.com>
2023-06-14 17:53:52 -0400

    elimino archivos duplicados
    elimino archivos duplicados

--- Contabilidad/BoletaElectronicaWeb - copia/src/cl/laaraucana/boletaelectronica/reportDao/ReportDao.java
+++
@@ -1,9 +0,0 @@
-package cl.laaraucana.boletaelectronica.reportDao;
-
-import java.sql.Connection;
-
-public interface ReportDao {
-	
-	public Connection getConnection() throws Exception;  
-
-}
--- Contabilidad/BoletaElectronicaWeb - copia/src/cl/laaraucana/boletaelectronica/services/MigrateService.java
+++
@@ -1,7 +0,0 @@
-package cl.laaraucana.boletaelectronica.services;
-
-public interface MigrateService {
-	
-	public void migrate(String id) throws Exception;
-
-}
