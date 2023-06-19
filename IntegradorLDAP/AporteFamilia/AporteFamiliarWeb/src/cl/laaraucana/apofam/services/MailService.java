2e2cdc4ac6e541a10da66ed8ff31e35b748d6bdc
hurojas-cyber <hugo.rojas.r@gmail.com>
2023-06-19 12:13:07 -0400

    elimino proyecto del repositorio
    elimino proyecto del repositorio

--- IntegradorLDAP/AporteFamilia/AporteFamiliarWeb/src/cl/laaraucana/apofam/services/MailService.java
+++
@@ -1,8 +0,0 @@
-package cl.laaraucana.apofam.services;
-
-public interface MailService {
-
-	public boolean sendEmailClie(String to, String subject, String bodyContent, String rut, String ruta) throws Exception;
-	
-	
-}
